1f9d08e8e494a294683ec224698276b5fda550b7
David Bland <davidbland@hotmail.com>
2020-10-27 15:37:06 -0400

    Updates

---
+++ precache-manifest.1344b278f4e5c9ea34a3234051813891.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53df457c4e9d34bdfd453c8b768a1cb",
+    "url": "/faux-dashboard/index.html"
+  },
+  {
+    "revision": "59f5c3f91709efb6aa81",
+    "url": "/faux-dashboard/static/css/main.b7d357d0.chunk.css"
+  },
+  {
+    "revision": "4e2f6bf72b6c34596f1a",
+    "url": "/faux-dashboard/static/js/2.db162488.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/faux-dashboard/static/js/2.db162488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f5c3f91709efb6aa81",
+    "url": "/faux-dashboard/static/js/main.01281af5.chunk.js"
+  },
+  {
+    "revision": "729aaf46b5e6dfcfc9f5",
+    "url": "/faux-dashboard/static/js/runtime-main.a60a1c22.js"
+  }
+]);
