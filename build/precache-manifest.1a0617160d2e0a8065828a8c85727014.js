6c2cdaaec34912417996d74859fd3cdf8ed781e1
David Bland <davidbland@hotmail.com>
2020-10-27 15:17:38 -0400

    including build version for demo

---
+++ build/precache-manifest.1a0617160d2e0a8065828a8c85727014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6cacd9e7ba55b770a3d5327b72f914b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "09ee3cfaf6dc1a761305",
+    "url": "./static/css/main.b7d357d0.chunk.css"
+  },
+  {
+    "revision": "4e2f6bf72b6c34596f1a",
+    "url": "./static/js/2.db162488.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "./static/js/2.db162488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ee3cfaf6dc1a761305",
+    "url": "./static/js/main.28d93a60.chunk.js"
+  },
+  {
+    "revision": "8695b68883c4d2105bf3",
+    "url": "./static/js/runtime-main.cb4a5c3f.js"
+  }
+]);
